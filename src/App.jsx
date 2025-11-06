2380ae96148bc62533a9c9536d07d59a5e9b2b53
HASHIR AHMAD <hashirahmad809@gmail.com>
2025-11-06 16:09:06 +0500

    Fix file paths for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import Navbar from "./components/Navbar.jsx";
-import Hero from "./components/Hero.jsx";
-import StatsSection from "./components/StatsSection.jsx";
-import Services from "./components/Services.jsx";
-import Choose from "./components/Choose.jsx";
-
+import Navbar from "./components/ui/Navbar.jsx";
+import Hero from "./components/ui/Hero.jsx";
+import StatsSection from "./components/ui/StatsSection.jsx";
+import Services from "./components/ui/Services.jsx";
+import Choose from "./components/ui/Choose.jsx";
 
 function App() {
   return (
